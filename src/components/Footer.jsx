c72adf984687d87b8deb0c393218e90fb29c93a0
GODCarNageOP <yashvharale@gmail.com>
2024-07-10 16:59:01 +0530

    refactor(footer.jsx): new icons added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Section from "./Section";
-import { socials } from "../constants";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -11,16 +15,21 @@ const Footer = () => {
         </p>
 
         <ul className="flex gap-5 flex-wrap">
-          {socials.map((item) => (
-            <a
-              key={item.id}
-              href={item.url}
-              target="_blank"
-              className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6"
-            >
-              <img src={item.iconUrl} width={16} height={16} alt={item.title} />
-            </a>
-          ))}
+          <a className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6">
+            <FaLinkedin size={16} />
+          </a>
+          <a className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6">
+            <FaInstagram size={16} />
+          </a>{" "}
+          <a className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6">
+            <FaGithub size={16} />
+          </a>{" "}
+          <a className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6">
+            <FaFacebook size={16} />
+          </a>
+          <a className="flex items-center justify-center w-10 h-10 bg-n-7 rounded-full transition-colors hover:bg-n-6">
+            <FaXTwitter size={16} />
+          </a>
         </ul>
       </div>
     </Section>
